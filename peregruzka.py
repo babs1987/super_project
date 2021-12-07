6f107ac613fe5efe7446b9a8eaf5515ac83a7797
HOME <egorsintez@mail.ru>
2021-12-07 20:46:15 +0700

    Перегрузка

---
+++ peregruzka.py
@@ -0,0 +1,11 @@
+class VinilMarket(object):
+    def __init__(self,name,date,genre):
+        self.name=name
+        self.date=date
+        self.genre=genre
+
+    def __str__(self):
+        return f"Название пластинки {self.name}, Дата Поступления {self.date}, Жанр музыки {self.genre}"
+
+    def __add__(self, other):
+        return VinilMarket(self.name + other,self.date+other,self.genre+other)
