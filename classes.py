fef8dda76eeec178bb6d9a28510322a16a468971
ivan_ <ivan_volodin_357@mail.ru>
2021-12-07 21:49:45 +0700

    Я добавил классы автомобиль и человек

---
+++ classes.py
@@ -0,0 +1,42 @@
+# Модуль в котором находятся два класса Car и Human.
+# Заполните классы как считаете нужными 2-3 метода достаточно
+
+
+
+
+class car(object):
+    def name(self):
+        print("car 1")
+    def length(self):
+        print(210)
+    def width(self):
+        print(140)
+    def height(self):
+        print(180)
+    def engine_type(self):
+        print("disel")
+    def engine_stroke(self):
+        print(4)
+    def doors(self):
+        print(4)
+    def color(self):
+        print("silver")
+    def power(self):
+        print(12345678)
+
+
+class human(object):
+    def name(self):
+        print("oleg")
+    def height(self):
+        print(175)
+    def weight(self):
+        print(70)
+    def specialization(self):
+        print("street cleaner")
+
+
+
+
+
+
