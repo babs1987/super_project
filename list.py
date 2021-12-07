3f61b56e95e35d7b832b2b86eba3d0d777b070e1
babs1987 <erlinyuriy@gmail.com>
2021-12-07 22:03:33 +0700

    super puper

--- list.py
+++ list.py
@@ -1,8 +1,10 @@
 class lst():
+    @staticmethod
     def list_create(N):
         lt = [i for i in range(N)]
         return lt
 
+    @staticmethod
     def list_createAB(N, a, b):
         list = [i for i in range(a, b + 1)]
         return list
--- main.py
+++ main.py
@@ -1,23 +1,13 @@
+import peregruzka,Mathh, list,classes
 
-import Mathh
+s=peregruzka.VinilMarket("goga","20.20.21","triller")
+print(s)
 print(Mathh.Math.maximum(1,2,3,4))
+list.lst.list_create()
 
 
 
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
