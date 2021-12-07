90b8aaa03b2f091c65253a7c24e36a06fc1a02ef
1 <moji.flin@yandex.ru>
2021-12-07 21:53:09 +0700

    списки

---
+++ list.py
@@ -0,0 +1,8 @@
+class lst():
+    def list_create(N):
+        lt = [i for i in range(N)]
+        return lt
+
+    def list_createAB(N, a, b):
+        list = [i for i in range(a, b + 1)]
+        return list
